94ad83ebb0595b06cbb97d996f836da48f0622ae
Artem Shapiro <artemshapa1772@gmail.com>
2017-01-21 21:45:18 +0200

    Updates

--- src/components/NewGame.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default (props) =>
-  <button className='btn btn-default' onClick={() => props.onClick()}>
-    New Game
-  </button>
--- src/components/Square.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Square.css'
-
-export default (props) => (
-  <button className='square' onClick={() => props.onClick()}>
-    {props.value}
-  </button>
-)
