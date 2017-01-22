e2e8ca263b5047e252363012c53c585206f4cc7f
Artem Shapiro <artemshapa1772@gmail.com>
2017-01-22 13:42:45 +0200

    Adding statistics. Refactor/Fix around game storage

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './GameInfo.css'
 
 export default class extends React.Component {
   render () {
@@ -12,6 +13,18 @@ export default class extends React.Component {
       status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O')
     }
 
-    return <div className='status'>{status}</div>
+    return (
+      <div className='clearfix'>
+        <div className='status'>
+          {status}
+        </div>
+
+        <div className='statistic col-sm-4 col-sm-offset-4'>
+          <div className='col-sm-4'><p>Player (X)</p> {this.props.statistic.xWins}</div>
+          <div className='col-sm-4'><p>Draws </p> {this.props.statistic.draws}</div>
+          <div className='col-sm-4'><p>Player (O) </p> {this.props.statistic.oWins}</div>
+        </div>
+      </div>
+    )
   }
 }
